5ed6593bd0d2d6a2ac468f74ea5db3b8b831c9b4
lobeme <139631950+lobeme@users.noreply.github.com>
2025-07-22 03:23:22 +0000

    work done for day 2

--- Part1/ex07.py
+++ Part1/ex07.py
@@ -11,14 +11,23 @@ age = {'Hans': 24, 'Prag': 23, 'Bunyod': 18}
 
 # Insert your code
 
-wholedictionary = {'Hans': 24, 'Prag': 23, 'Bunyod': 18}
-print(wholedictionary)
+# Dictionary containing names and ages
+age = {'Hans': 24, 'Prag': 23, 'Bunyod': 18}
 
-age of 'Hans' = {24}
-print(age of 'Hans')
+# 1. Print the whole dictionary
+print(age)
 
-updated age of 'Prag' = {30}
-print(updated age of 'Prag')
+# 2. Print the age of 'Hans'
+print(age['Hans'])
 
-de1 age{'Bunyod'}
+# 3. Change the age of 'Prag' to 30
+age['Prag'] = 30
 
+# 4. Print the updated age of 'Prag'
+print(age['Prag'])
+
+# 5. Delete key-value pair of 'Bunyod' from the dictionary
+del age['Bunyod']
+
+# 6. Print the updated dictionary
+print(age)
